58d93fd610f200439ae7af109b8cf62a30f1df67
Naveen Jayaram <naveenj@infinite.com>
2023-09-14 14:37:08 +0530

    app changes

--- Email_notification_service/src/main/java/com/microservice/notification/NotificationServiceApplication.java
+++ Email_notification_service/src/main/java/com/microservice/notification/NotificationServiceApplication.java
@@ -2,14 +2,13 @@ package com.microservice.notification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient
 @SpringBootApplication
 public class NotificationServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NotificationServiceApplication.class, args);
+		System.out.println("NOTIFICATION-SERVICE IS UP AND RUNNING");
 	}
 
 }
--- product_service/src/main/java/com/microservice/product/ProductServiceApplication.java
+++ product_service/src/main/java/com/microservice/product/ProductServiceApplication.java
@@ -3,11 +3,9 @@ package com.microservice.product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableEurekaClient
 @EnableSwagger2
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class ProductServiceApplication {
